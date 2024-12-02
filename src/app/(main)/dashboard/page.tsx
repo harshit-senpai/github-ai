e6a7d5cd617fb3a546b456b90f07d760934cc07e
harshit Mishra <miananya2@gmail.com>
2024-12-03 03:40:33 +0530

    dashboard page

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,4 +1,35 @@
+"use client";
+
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
+import useProject from "~/hooks/useProjects";
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  const { project } = useProject();
+
+  return (
+    <main>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        <div className="w-fit rounded-md bg-primary px-4 py-3">
+          <div className="flex items-center">
+            <Github className="size-4 text-white" />
+            <div className="ml-2">
+              <p className="text-sm font-medium text-white">
+                This project is linked to{" "}
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  className="inline-flex items-center text-white/80 hover:underline"
+                  target="_blank"
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className="ml-2 size-4" />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 export default DashboardPage;
